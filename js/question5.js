fc88d5df73f38115e62a02871832f0478be35ef9
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-27 06:18:26 -0800

    finished question 5

--- js/question5.js
+++ js/question5.js
@@ -19,18 +19,70 @@ let animal = ""
 let verb = ""
 let noun = ""
 let pluralNoun = ""
-let location = ""
+let loc = ""
 
 madInput1.addEventListener('keydown', function(e){
 
- if (e.key === 'Enter')
+ if (e.key === "Enter")
  {
-    
+    place = madInput1.value.trim()
  }
 
 })
 
+madInput2.addEventListener('keydown', function(e){
 
+    if (e.key === "Enter")
+    {
+       adjective = madInput2.value.trim()
+    }
+   
+})
+
+madInput3.addEventListener('keydown', function(e){
+
+    if (e.key === "Enter")
+    {
+       animal = madInput3.value.trim()
+    }
+   
+})
+   
+madInput4.addEventListener('keydown', function(e){
+   
+    if (e.key === "Enter")
+    {
+      verb = madInput4.value.trim()
+    }
+      
+})
+
+madInput5.addEventListener('keydown', function(e){
+
+    if (e.key === "Enter")
+    {
+       noun = madInput5.value.trim()
+    }
+   
+})
+   
+madInput6.addEventListener('keydown', function(e){
+   
+   if (e.key === "Enter")
+   {
+      pluralNoun = madInput6.value.trim()
+   }
+      
+})
+   
+madInput7.addEventListener('keydown', function(e){
+   
+   if (e.key === "Enter")
+   {
+      loc = madInput7.value.trim()
+   }
+      
+})
 
 
 madLibBtn.addEventListener('click', async function(){
@@ -39,7 +91,7 @@ madLibBtn.addEventListener('click', async function(){
     const data = await promise.json();
     console.log(data)
 
-
+    madLib.innerText = `${data.value[0]}${place} ${data.value[2]}${adjective} ${data.value[4]}${animal} ${data.value[6]}${verb} ${data.value[8]}${noun} ${data.value[10]}${pluralNoun} ${data.value[12]}${loc} ${data.value[14]}.`
 
 
 })
